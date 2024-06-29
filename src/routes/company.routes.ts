2d787ebec73bc2e854a4577a87066eb713ace6b6
MarcosTenorioDev <marcosvtenoriodev@gmail.com>
2024-06-29 19:06:40 -0300

    Ajustes na formatação do arquivo de routes de company

--- src/routes/company.routes.ts
+++ src/routes/company.routes.ts
@@ -4,18 +4,25 @@ import { CompanyRepositoryPrisma } from "../repositories/company.repositories";
 import { CompanyUseCase } from "../usecases/company.usecase";
 import { UserRepositoryPrisma } from "../repositories/user.repositories";
 
-export async function companyRoutes(fastify: FastifyInstance) {
-	const companyRepositoryPrisma = new CompanyRepositoryPrisma();
-    const userRepositoryPrisma = new UserRepositoryPrisma();
-	const companyUseCase = new CompanyUseCase(companyRepositoryPrisma, userRepositoryPrisma);
+const companyRepositoryPrisma = new CompanyRepositoryPrisma();
+const userRepositoryPrisma = new UserRepositoryPrisma();
+const companyUseCase = new CompanyUseCase(
+	companyRepositoryPrisma,
+	userRepositoryPrisma
+);
 
+export async function companyRoutes(fastify: FastifyInstance) {
+    CreateCompanyRoute(fastify)
+}
+
+function CreateCompanyRoute(fastify: FastifyInstance) {
 	fastify.post<{ Body: CompanyCreate }>("/", async (req, reply) => {
-        const {name, ownerId} = req.body
-        try {
-            const data = await companyUseCase.create({name, ownerId});
-            reply.code(201).send(data);
-        } catch (error) {
-            reply.code(400).send(error);
-        }
-    })
+		const { name, ownerId } = req.body;
+		try {
+			const data = await companyUseCase.create({ name, ownerId });
+			reply.code(201).send(data);
+		} catch (error) {
+			reply.code(400).send(error);
+		}
+	});
 }
